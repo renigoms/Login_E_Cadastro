ff1828fc781def60c3e6c27e859aa4b2bc65dc06
renan <renan.nic28@gmail.com>
2023-01-01 22:05:54 -0300

    teste4

--- src/view/TelaDeCadastroView.java
+++ src/view/TelaDeCadastroView.java
@@ -31,6 +31,11 @@ public class TelaDeCadastroView extends Controller.TelaController{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Vamos ver o que deu com essa merda.");
+				System.out.println("E la vamos nois");
+				if (getLoginText().getText().equalsIgnoreCase("") || getPasswordText().getText().equalsIgnoreCase("")) {
+					System.out.println("Deu certo");
+					
+				}
 				UsuarioController usuario1 = new UsuarioController();
 				usuario1.getUsuario().setLogin(getLoginText());
 				usuario1.getUsuario().setSenha(getPasswordText());
